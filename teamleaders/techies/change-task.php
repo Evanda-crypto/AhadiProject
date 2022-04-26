5ca96c8228bcfe3d02d524aeb33d9d686af9145d
Evanda-crypto <evanceodhiambo123@gmail.com>
2022-04-26 17:00:58 +0300

    commit

--- teamleaders/techies/change-task.php
+++ teamleaders/techies/change-task.php
@@ -149,10 +149,10 @@ if(isset($_POST['submit'])){
     <!-- Right Panel -->
     <div id="right-panel" class="right-panel">
         <!-- Header-->
-        <header id="header" class="header">
+        <header id="header" class="header" style="height: 65px;">
             <div class="top-left">
                 <div class="navbar-header">
-                <img src="../../images/picture1.png" style="width: 120px; height: 70px;" class="logo-icon" alt="logo icon">
+                <img src="../../images/picture1.png" style="width: 120px; height: 60px;" class="logo-icon" alt="logo icon">
                     <a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a>
                 </div>
             </div>
