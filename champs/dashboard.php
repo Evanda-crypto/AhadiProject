870681c766fd298abbf39c4d1020ad774a58df1c
Evanda-crypto <evanceodhiambo123@gmail.com>
2022-06-03 19:58:37 +0300

    champs dashboard

--- champs/dashboard.php
+++ champs/dashboard.php
@@ -213,10 +213,7 @@ if (!$connection) {
                       reminders on reminders.ClientID=papdailysales.ClientID LEFT JOIN techietask on techietask.ClientID=papdailysales.ClientID left join 
                       papnotinstalled on papnotinstalled.ClientID=papdailysales.ClientID WHERE papdailysales.PapStatus<>'Retrieved' AND papdailysales.PapStatus<>'Restored' and
                      papnotinstalled.ClientID is null and papinstalled.ClientID is null and techietask.ClientID is null and reminders.ClientID is null and papdailysales.ChampName='" .
-                      $_SESSION["FName"] .
-                      " " .
-                      $_SESSION["LName"] .
-                      "'";
+                      $_SESSION["FName"]." ". $_SESSION["LName"]."'";
                   $data = mysqli_query($connection, $query);
                   while ($row = mysqli_fetch_assoc($data)) {
                       echo $row["notinstalled"] . "<br><br>";
@@ -241,11 +238,7 @@ if (!$connection) {
                                         <div class="text-left dib">
                                             <div class="stat-text"><span class="count"><?php
                   $query =
-                      "SELECT COUNT(*) as torestore from papnotinstalled WHERE ChampName='" .
-                      $_SESSION["FName"] .
-                      " " .
-                      $_SESSION["LName"] .
-                      "'";
+                      "SELECT COUNT(*) as torestore from papnotinstalled WHERE ChampName='".$_SESSION["FName"]." ".$_SESSION["LName"]."'";
                   $data = mysqli_query($connection, $query);
                   while ($row = mysqli_fetch_assoc($data)) {
                       echo $row["torestore"] . "<br><br>";
@@ -270,11 +263,7 @@ if (!$connection) {
                                         <div class="text-left dib">
                                             <div class="stat-text"><span class="count"><?php
                   $query =
-                      "SELECT count(*) as turnedon from papdailysales where PapStatus='Turned On' and ChampName='" .
-                      $_SESSION["FName"] .
-                      " " .
-                      $_SESSION["LName"] .
-                      "'";
+                      "SELECT count(*) as turnedon from papdailysales where PapStatus='Turned On' and ChampName='".$_SESSION["FName"]." ".$_SESSION["LName"]."'";
                   $data = mysqli_query($connection, $query);
                   while ($row = mysqli_fetch_assoc($data)) {
                       echo $row["turnedon"] . "<br><br>";
@@ -299,11 +288,7 @@ if (!$connection) {
                                         <div class="text-left dib">
                                             <div class="stat-text"><span class="count"><?php
                   $query =
-                      "SELECT count(*) as allpap from papdailysales left join papnotinstalled on papnotinstalled.ClientID=papdailysales.ClientID where papdailysales.PapStatus<>'Retrieved' and papnotinstalled.ClientID is null and papdailysales.ChampName='" .
-                      $_SESSION["FName"] .
-                      " " .
-                      $_SESSION["LName"] .
-                      "'";
+                      "SELECT count(*) as allpap from papdailysales left join papnotinstalled on papnotinstalled.ClientID=papdailysales.ClientID where papdailysales.PapStatus<>'Retrieved' and papnotinstalled.ClientID is null and papdailysales.ChampName='".$_SESSION["FName"]." ".$_SESSION["LName"]."'";
                   $data = mysqli_query($connection, $query);
                   while ($row = mysqli_fetch_assoc($data)) {
                       echo $row["allpap"] . "<br><br>";
