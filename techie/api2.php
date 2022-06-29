5f9fe8b23568bff25ab686445ecb3ae4bd4f6701
KONNECT <evanceodhiambo123@gmail.com>
2022-06-29 13:35:24 +0300

    mtr api interaction

--- techie/api2.php
+++ techie/api2.php
@@ -1,30 +1,43 @@
 <?php
-if(isset($_POST['submit']) && isset($_FILES['Meter_Picture']['tmp_name'])){
+
 require_once __DIR__ . '../../vendor/autoload.php';
+
+use GuzzleHttp\Client;
+if(isset($_POST['submit']) && isset($_FILES['Meter_Picture']['tmp_name'])){
+
+
 $client = new GuzzleHttp\Client();
+
+$client = new Client([
+    // Base URI is used with relative requests
+    'base_uri' => 'https://reqres.in',
+]);
+
 $response = $client->request('POST', 'http://app.sasakonnect.net:19003/api/Meters/', [
-    'multipart' => [
-        [
-            'Meter_Picture'     => basename($_FILES['Meter_Picture']['tmp_name']),
-            'Meter_Picture' => fopen(basename($_FILES['Meter_Picture']['name']), 'r')
-        ],
-        [
-            'Cluster_name' => $_POST["Cluster_name"],
-  
-            'Meter_Number'  => $_POST["Meter_Number"],
-    
-            'Contact_number'    => $_POST["Contact_number"],
-    
-            'date_Installed' => $_POST["date_Installed"],
-    
-            'Region'  => $_POST["Region"],
-    
-            'Techie_team'    => $_POST["Techie_team"],
-    
-            'Contact_Person'    => $_POST["Contact_Person"],
-    
-            'Comments'    => $_POST["Comments"]
-        ]
+    'json' => [
+        'Cluster_name' => $_POST["Cluster_name"],
+
+        'Meter_Number'  => $_POST["Meter_Number"],
+
+        'Contact_number'    => $_POST["Contact_number"],
+
+        'date_Installed' => $_POST["date_Installed"],
+
+        'Region'  => $_POST["Region"],
+
+        'Techie_team'    => $_POST["Techie_team"],
+
+        'Contact_Person'    => $_POST["Contact_Person"],
+
+        'Comments'    => $_POST["Comments"]
     ]
 ]);
+
+
+//get status code using $response->getStatusCode();
+
+$body = $response->getBody();
+$arr_body = json_decode($body);
+print_r($arr_body);
 }
+
