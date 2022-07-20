8fb4b6cf6be695eb7ec647fa7c0078d86cd44376
Evanda-crypto <evanceodhiambo123@gmail.com>
2022-07-20 12:09:33 +0300

    dev reports

--- nats/fill_developers_report.php
+++ nats/fill_developers_report.php
@@ -246,17 +246,17 @@ date_default_timezone_set("Africa/Nairobi");
                                             </div>
                                         <div class="form-group has-success">
                                         <strong><label for="cc-name" class="control-label mb-1">Milestones<span style="color: #FF0000" >*</span></label></strong>
-                                                <input id="milestone" name="key_milestone" type="text" class="form-control cc-name valid" data-val="true" placeholder="Key Milestone" required >
+                                                <input id="milestones" name="key_milestone" type="text" class="form-control cc-name valid" data-val="true" placeholder="Key Milestone" required >
                                                 <span class="help-block field-validation-valid" data-valmsg-for="cc-name" data-valmsg-replace="true"></span>
                                             </div>
                                             <strong><div class="form-group">
-                                                <label for="cc-number" class="control-label mb-1">Completed Tasks</label></strong>
-                                                <textarea  id="editor1" name="editor1" type="text" class="form-control cc-number identified visa"required  placeholder="Completed Tasks"></textarea>
+                                                <label for="cc-number" class="control-label mb-1">Completed Tasks<span style="color: #FF0000" >*</span></label></strong>
+                                                <textarea  id="editor1" name="completed_tasks" type="text" class="form-control cc-number identified visa"required  placeholder="Completed Tasks"></textarea>
                                                 <span class="help-block" data-valmsg-for="cc-number" data-valmsg-replace="true"></span>
                                             </div>
                                             <div class="form-group">
-                                            <strong><label for="cc-number" class="control-label mb-1">Ongoing Tasks</label></strong>
-                                                <textarea id="buildings" required name="ongoing_tasks" type="text" class="form-control"  placeholder="Ongoing Tasks"></textarea>
+                                            <strong><label for="cc-number" class="control-label mb-1">Ongoing Tasks<span style="color: #FF0000" >*</span></label></strong>
+                                                <textarea id="editor2" required name="ongoing_tasks" type="text" class="form-control"  placeholder="Ongoing Tasks"></textarea>
                                                 <span class="help-block" data-valmsg-for="cc-number" data-valmsg-replace="true"></span>
                                             </div>
 
@@ -471,6 +471,8 @@ function dateDiff() {
       editor.focus();
     }
   </script>
+
+
   <script>
     // Attaching event listeners to the global CKEDITOR object.
     // The instanceReady event is fired when an instance of CKEditor 4 has finished its initialization.
@@ -483,7 +485,253 @@ function dateDiff() {
 
     // Replace the <textarea id="editor1"> with a CKEditor 4 instance.
     // A reference to the editor object is returned by CKEDITOR.replace() allowing you to work with editor instances.
-    var editor = CKEDITOR.replace('editor1', {
+    var editor = CKEDITOR.replace('editor2', {
+      height: 150,
+      removeButtons: 'PasteFromWord'
+    });
+
+    // Attaching event listeners to CKEditor 4 instances.
+    // Refer to https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html for a list of all available events.
+    editor.on('focus', function(evt) {
+      ShowMessage('Editor instance <em>' + this.name + '</em> <b>is focused</b>.');
+    });
+    editor.on('blur', function(evt) {
+      ShowMessage('Editor instance <em>' + this.name + '</em> <b>lost focus</b>.');
+    });
+    // Helper variable to count the number of detected changes in CKEditor 4.
+    var changesNum = 0;
+    editor.on('change', function(evt) {
+      ShowMessage('The number of changes in <em>' + this.name + '</em>: <b>' + ++changesNum + '</b>.');
+    });
+  </script>
+  <script>
+    // Helper function to display messages below CKEditor 4.
+    function ShowMessage(msg) {
+      document.getElementById('eMessage').innerHTML = msg;
+    }
+
+    function InsertHTML() {
+      // Get the editor instance that you want to interact with.
+      var editor = CKEDITOR.instances.editor2;
+      var value = document.getElementById('htmlArea').value;
+
+      // Check the active editing mode.
+      if (editor.mode == 'wysiwyg') {
+        // Insert HTML code.
+        // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html#method-insertHtml
+        editor.insertHtml(value);
+      } else
+        alert('You must be in WYSIWYG mode!');
+    }
+
+    function InsertText() {
+      // Get the editor instance that you want to interact with.
+      var editor = CKEDITOR.instances.editor2;
+      var value = document.getElementById('txtArea').value;
+
+      // Check the active editing mode.
+      if (editor.mode == 'wysiwyg') {
+        // Insert as plain text.
+        // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html#method-insertText
+        editor.insertText(value);
+      } else
+        alert('You must be in WYSIWYG mode!');
+    }
+
+    function SetContents() {
+      // Get the editor instance that you want to interact with.
+      var editor = CKEDITOR.instances.editor2;
+      var value = document.getElementById('htmlArea').value;
+
+      // Set editor content (replace current content).
+      // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html#method-setData
+      editor.setData(value);
+    }
+
+    function GetContents() {
+      // Get the editor instance that you want to interact with.
+      var editor = CKEDITOR.instances.editor2;
+
+      // Get editor content.
+      // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html#method-getData
+      alert(editor.getData());
+    }
+
+    function ExecuteCommand(commandName) {
+      // Get the editor instance that you want to interact with.
+      var editor = CKEDITOR.instances.editor2;
+
+      // Check the active editing mode.
+      if (editor.mode == 'wysiwyg') {
+        // Execute the command.
+        // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html#method-execCommand
+        editor.execCommand(commandName);
+      } else
+        alert('You must be in WYSIWYG mode!');
+    }
+
+    function CheckDirty() {
+      // Get the editor instance that you want to interact with.
+      var editor = CKEDITOR.instances.editor2;
+      // Checks whether the current editor content contains changes when compared
+      // to the content loaded into the editor at startup.
+      // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html#method-checkDirty
+      alert(editor.checkDirty());
+    }
+
+    function ResetDirty() {
+      // Get the editor instance that you want to interact with.
+      var editor = CKEDITOR.instances.editor2;
+      // Resets the "dirty state" of the editor.
+      // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html#method-resetDirty
+      editor.resetDirty();
+      alert('The "IsDirty" status was reset.');
+    }
+
+    function Focus() {
+      // Get the editor instance that you want to interact with.
+      var editor = CKEDITOR.instances.editor2;
+      // Focuses the editor.
+      // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html#method-focus
+      editor.focus();
+    }
+  </script>
+  <script>
+    // Attaching event listeners to the global CKEDITOR object.
+    // The instanceReady event is fired when an instance of CKEditor 4 has finished its initialization.
+    CKEDITOR.on('instanceReady', function(ev) {
+      ShowMessage('Editor instance <em>' + ev.editor.name + '</em> was loaded.');
+
+      // The editor is ready, so sample buttons can be displayed.
+      document.getElementById('eButtons').style.display = 'block';
+    });
+
+    // Replace the <textarea id="editor2"> with a CKEditor 4 instance.
+    // A reference to the editor object is returned by CKEDITOR.replace() allowing you to work with editor instances.
+    var editor = CKEDITOR.replace('editor2', {
+      height: 150,
+      removeButtons: 'PasteFromWord'
+    });
+
+    // Attaching event listeners to CKEditor 4 instances.
+    // Refer to https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html for a list of all available events.
+    editor.on('focus', function(evt) {
+      ShowMessage('Editor instance <em>' + this.name + '</em> <b>is focused</b>.');
+    });
+    editor.on('blur', function(evt) {
+      ShowMessage('Editor instance <em>' + this.name + '</em> <b>lost focus</b>.');
+    });
+    // Helper variable to count the number of detected changes in CKEditor 4.
+    var changesNum = 0;
+    editor.on('change', function(evt) {
+      ShowMessage('The number of changes in <em>' + this.name + '</em>: <b>' + ++changesNum + '</b>.');
+    });
+  </script>
+<script>
+    // Helper function to display messages below CKEditor 4.
+    function ShowMessage(msg) {
+      document.getElementById('eMessage').innerHTML = msg;
+    }
+
+    function InsertHTML() {
+      // Get the editor instance that you want to interact with.
+      var editor = CKEDITOR.instances.editor2;
+      var value = document.getElementById('htmlArea').value;
+
+      // Check the active editing mode.
+      if (editor.mode == 'wysiwyg') {
+        // Insert HTML code.
+        // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html#method-insertHtml
+        editor.insertHtml(value);
+      } else
+        alert('You must be in WYSIWYG mode!');
+    }
+
+    function InsertText() {
+      // Get the editor instance that you want to interact with.
+      var editor = CKEDITOR.instances.milestones;
+      var value = document.getElementById('txtArea').value;
+
+      // Check the active editing mode.
+      if (editor.mode == 'wysiwyg') {
+        // Insert as plain text.
+        // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html#method-insertText
+        editor.insertText(value);
+      } else
+        alert('You must be in WYSIWYG mode!');
+    }
+
+    function SetContents() {
+      // Get the editor instance that you want to interact with.
+      var editor = CKEDITOR.instances.milestones;
+      var value = document.getElementById('htmlArea').value;
+
+      // Set editor content (replace current content).
+      // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html#method-setData
+      editor.setData(value);
+    }
+
+    function GetContents() {
+      // Get the editor instance that you want to interact with.
+      var editor = CKEDITOR.instances.milestones;
+
+      // Get editor content.
+      // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html#method-getData
+      alert(editor.getData());
+    }
+
+    function ExecuteCommand(commandName) {
+      // Get the editor instance that you want to interact with.
+      var editor = CKEDITOR.instances.milestones;
+
+      // Check the active editing mode.
+      if (editor.mode == 'wysiwyg') {
+        // Execute the command.
+        // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html#method-execCommand
+        editor.execCommand(commandName);
+      } else
+        alert('You must be in WYSIWYG mode!');
+    }
+
+    function CheckDirty() {
+      // Get the editor instance that you want to interact with.
+      var editor = CKEDITOR.instances.milestones;
+      // Checks whether the current editor content contains changes when compared
+      // to the content loaded into the editor at startup.
+      // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html#method-checkDirty
+      alert(editor.checkDirty());
+    }
+
+    function ResetDirty() {
+      // Get the editor instance that you want to interact with.
+      var editor = CKEDITOR.instances.milestones;
+      // Resets the "dirty state" of the editor.
+      // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html#method-resetDirty
+      editor.resetDirty();
+      alert('The "IsDirty" status was reset.');
+    }
+
+    function Focus() {
+      // Get the editor instance that you want to interact with.
+      var editor = CKEDITOR.instances.milestones;
+      // Focuses the editor.
+      // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html#method-focus
+      editor.focus();
+    }
+  </script>
+  <script>
+    // Attaching event listeners to the global CKEDITOR object.
+    // The instanceReady event is fired when an instance of CKEditor 4 has finished its initialization.
+    CKEDITOR.on('instanceReady', function(ev) {
+      ShowMessage('Editor instance <em>' + ev.editor.name + '</em> was loaded.');
+
+      // The editor is ready, so sample buttons can be displayed.
+      document.getElementById('eButtons').style.display = 'block';
+    });
+
+    // Replace the <textarea id="milestones"> with a CKEditor 4 instance.
+    // A reference to the editor object is returned by CKEDITOR.replace() allowing you to work with editor instances.
+    var editor = CKEDITOR.replace('milestones', {
       height: 150,
       removeButtons: 'PasteFromWord'
     });
